a26de8851779a315516b606d068ed7c0cb156b58
diretoaopython <diretoaopython@gmail.com>
2025-03-13 22:26:33 -0300

    Alteracao na funcoes

---
+++ dpyFuncoes.py
@@ -0,0 +1,92 @@
+import requests
+
+
+"""
+Para mais informações sobre como obter a chave, assista o vídeo abaixo
+06 - Aprenda Python com Flet criando a conta da API OpenWeatherMap
+https://youtu.be/GLqCFhPm-gU?si=xx5HVgchE888a_Ru
+"""
+apiKey = "digite sua chave"
+
+def obterCoordPorNome(apiKey,cidade,pais,limite=1):
+    """
+    07 - Aprenda Python com Flet criando a função da API de Geocodificação OpenWeatherMap
+    https://www.youtube.com/watch?v=CbACEn252Fs&list=PLi6TNT5J8PtXotYJBLypBTWoTW7OsXFP4&index=9
+
+    Exemplo de como utilizar a função
+    latitude,longitude = obterCoordPorNome(apiKey,"Goiânia", "BR")
+    print(latitude,longitude)
+    """
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={cidade},{pais}&limit={limite}&appid={apiKey}"
+    requisicao = requests.get(url).json()
+    return requisicao[0]["lat"],requisicao[0]["lon"]
+
+def obterCoordPorIpUsuario():
+    """
+    08 - Aprenda Python com Flet criando a função para retornar o IP do usuário
+    https://www.youtube.com/watch?v=EmkB5bZFWNY&list=PLi6TNT5J8PtXotYJBLypBTWoTW7OsXFP4&index=9
+
+    Exemplo de como utilizar a função
+    lat,lon = obterLocalPorIpUsuario()
+    """
+    url = f"https://ipinfo.io/"
+
+    resposta = requests.get(url)
+
+    requisicao = resposta.json()
+
+    coordenadas = requisicao['loc'].split(',')
+    latitude = coordenadas[0]
+    longitude = coordenadas[1]
+
+    cidade = requisicao.get('city', 'Cidade desconhecida')
+    regiao = requisicao.get('Região', 'Cidade desconhecida')
+    pais = requisicao.get('country', 'País desconhecido')
+
+    return [latitude,longitude,cidade,regiao,pais]
+
+def obterClimaPorNome(apiKey,cidade,pais):
+    """
+    09 - Aprenda Python com Flet criando a função para obter o clima atual usando a API OpenWeatherMap
+    https://www.youtube.com/watch?v=tKFeLXq_IzQ&list=PLi6TNT5J8PtXotYJBLypBTWoTW7OsXFP4&index=10
+
+    Exemplo de como usar a função
+    requisicao = obterClimaPorNome(apiKey,"Goiânia","BR")
+    print(requisicao)
+    print("=======================================================")
+    print(requisicao['weather'])
+    print("=======================================================")
+    print(requisicao['weather'][0])
+    print("=======================================================")
+    print(requisicao['weather'][0]['description'])
+    """
+    latitude, longitude = obterCoordPorNome(apiKey, cidade, pais)
+
+    # Vídeo 09
+    # url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}"
+
+    # Vídeo 11
+    # https://www.youtube.com/watch?v=ZP2nXf1Ujtc&list=PLi6TNT5J8PtXotYJBLypBTWoTW7OsXFP4&index=12
+    # Adicionado o parâmetro units, que define o formato do dado da temperatura
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={apiKey}"
+    resposta = requests.get(url)
+    return resposta.json()
+
+def obterPrevisaoPorNome(apiKey,cidade,pais):
+    """
+    10 - Aprenda Python com Flet criando a função que retorna a previsão do do tempo
+    https://www.youtube.com/watch?v=3CWzf50UjQ0&list=PLi6TNT5J8PtXotYJBLypBTWoTW7OsXFP4&index=11
+
+    Exemplo de uso
+    requisicao = obterPrevisaoPorNome(apiKey,"Goiânia", "BR")
+    print(requisicao)
+    """
+    latitude, longitude = obterCoordPorNome(apiKey, cidade, pais)
+
+    url = f"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&appid={apiKey}"
+
+    # Usando uma data específica (Api de previsão que pode ser usada se você tiver uma conta de estudante de uma universidade)
+    # https://history.openweathermap.org/data/2.5/history/city?lat=41.85&lon=-87.65&appid={API key}
+    # url = f"https://history.openweathermap.org/data/2.5/history/city?lat={latitude}&lon={longitude}&start=1732492800&end=1732881600&units=metric&appid={apiKey}"
+    resposta = requests.get(url)
+    return resposta.json()
---
+++ main.py
@@ -0,0 +1,246 @@
+from datetime import datetime
+import flet as ft
+import dpyFuncoes
+
+
+def main(page: ft.Page):
+    def atualizarClima(e):
+        ###########################################################
+        # Obter o clima atual e visualizar no container central
+        ###########################################################
+        cidade, pais = pesqLocal.value.split(',')
+
+        ## Clima atual
+        dadosClima = funcoes.obterClimaPorNome(funcoes.apiKey, cidade=cidade, pais=pais)
+
+        cntCentralLinha1.controls[0].value = dadosClima['name']
+        cntCentralLinha2.controls[0].src =  f"https://openweathermap.org/img/wn/{dadosClima['weather'][0]['icon']}.png"
+        cntCentralLinha3.controls[0].value = f"{int(dadosClima['main']['temp'])} °"
+        cntCentralLinha4.controls[0].value = f"{dadosClima['main']['humidity']} %"
+
+        ## Previsão do tempo
+        # 1. Vamos obter os dados da previsão através da função obterPrevisaoPorNome
+        dadosPrevisao = funcoes.obterPrevisaoPorNome(funcoes.apiKey, cidade=cidade, pais=pais)
+
+        # 2. Atribuir os valores padrão ao dicionário que irá receber os dados do container inferior
+        # tempMaxDia = {"2024-11-23": ("Seg", "10n", 28), "2024-11-24": ("Ter", "10d", 32),}
+        from collections import defaultdict
+        tempMaxDia = defaultdict(lambda: ("", "", float('-inf')))
+
+        # 3. Buscar e tratar os dados que serão visualizados no container inferior
+        previsoes = dadosPrevisao['list']
+        for previsao in previsoes:
+            # Armazenar dia e dia da semana
+            data = datetime.fromtimestamp(previsao['dt'])
+            dias = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']
+            diaTmp = dias[data.weekday()]
+            dia = data.strftime('%Y/%m/%d')
+
+            # Tupla de elementos
+            # Ícone
+            iconTmp = previsao['weather'][0]['icon']
+
+            # Temperatura
+            tempTmp = int(previsao['main']['temp'])
+
+            if iconTmp.endswith('d'):
+                if tempTmp > tempMaxDia[dia][2]:
+                    tempMaxDia[dia] = (diaTmp, iconTmp, tempTmp)
+
+        # 4. Exibir os dados no container inferior
+
+        idxColuna = 0
+        for item in tempMaxDia:
+            if idxColuna <= 4:
+                cntInferiorLinha1.controls[idxColuna].value = tempMaxDia[item][0]
+                cntInferiorLinha2.controls[idxColuna].src = f"https://openweathermap.org/img/wn/{tempMaxDia[item][1]}.png"
+                cntInferiorLinha3.controls[idxColuna].value = f"{tempMaxDia[item][2]} °"
+
+                idxColuna += 1
+
+        page.update()
+
+    def obterLocalUsuario(e):
+        listaInfIp = funcoes.obterCoordPorIpUsuario()
+
+        # [latitude,longitude,cidade,regiao,pais]
+        cidade = listaInfIp[2]
+        pais = listaInfIp[4]
+        pesqLocal.value = f"{cidade}, {pais}"
+        pesqLocal.update()
+
+        atualizarClima(e)
+
+
+    page.bgcolor = ft.colors.BLACK
+    page.title = "Meu primeiro App em Python com Flet"
+    page.window.height = 720
+    page.window.width = 350
+
+    #######################################################################
+    cntPrincipal = ft.Container(
+        bgcolor="#3F6CB7",
+        border_radius=35,
+        height=page.window.height * 0.90,
+        padding=10,
+        width=page.window.width * 0.90,
+        content= ft.Column(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            height=page.window.height * 0.90,
+            width=page.window.width * 0.90,
+        )
+    )
+    #######################################################################
+    # Container Superior
+    #######################################################################
+    cntSuperior = ft.Container(
+        #bgcolor="white",
+        height=100,
+        width=cntPrincipal.width * 0.95,
+        content=ft.Row(
+        )
+    )
+
+    # Container Superior - Texto para pesquisar o local
+    pesqLocal = ft.TextField(
+        bgcolor="white",
+        border_color="white",
+        border_radius=10,
+        width=cntSuperior.width * 0.80,
+        hint_text="Informe a cidade e País",
+        hint_style=ft.TextStyle(
+            size=12,
+            color=ft.colors.BLUE_GREY,
+        ),
+        text_align=ft.TextAlign.CENTER,
+        text_style=ft.TextStyle(
+            size=14,
+            color=ft.colors.BLACK,
+        ),
+        value = "Goiânia, BR"
+
+    )
+    cntSuperior.content.controls.append(pesqLocal)
+
+    # Container Superior - Botão para executar a pesquisa
+    btnPesquisar = ft.IconButton(
+        icon=ft.icons.SEARCH,
+        on_click=atualizarClima,
+    )
+    cntSuperior.content.controls.append(btnPesquisar)
+
+    #######################################################################
+    # Container Central
+    #######################################################################
+    cntCentral = ft.Container(
+        height=200,
+        content=ft.Column(
+        ),
+    )
+
+    cntCentralLinha1 = ft.Row(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.Text(
+                "Goiânia",
+                font_family="Segaon Medium",
+                color="white",
+                size=38,
+                weight=ft.FontWeight.BOLD
+            ),
+        ],
+    )
+    cntCentral.content.controls.append(cntCentralLinha1)
+
+    cntCentralLinha2 = ft.Row(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.Image(
+                src="http://openweathermap.org/img/wn/02d.png",
+                width=50,
+                height=50,
+            )
+        ],
+    )
+    cntCentral.content.controls.append(cntCentralLinha2)
+
+    cntCentralLinha3 = ft.Row(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.Text(
+                "38°",
+                font_family="Segaon Medium",
+                color="white",
+                size=28,
+            )
+        ],
+    )
+    cntCentral.content.controls.append(cntCentralLinha3)
+
+    cntCentralLinha4 = ft.Row(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.Text(
+                "75%",
+                font_family="Segaon Medium",
+                color="white",
+            )
+        ],
+    )
+    cntCentral.content.controls.append(cntCentralLinha4)
+
+    #######################################################################
+    # Container Inferior
+    #######################################################################
+    cntInferior = ft.Container(
+        height=150,
+        content=ft.Column(
+        ),
+    )
+
+    cntInferiorLinha1 = ft.Row(
+        alignment=ft.MainAxisAlignment.SPACE_AROUND,
+        controls=[
+            ft.Text(
+                f"D{i}",
+                color="white",
+            ) for i in range(1,6)
+        ],
+    )
+    cntInferior.content.controls.append(cntInferiorLinha1)
+
+    cntInferiorLinha2 = ft.Row(
+        alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.Image(
+                src=f"http://openweathermap.org/img/wn/0{i}d.png",
+                width=50,
+                height=50,
+            ) for i in range(1,6)
+        ],
+    )
+    cntInferior.content.controls.append(cntInferiorLinha2)
+
+    cntInferiorLinha3 = ft.Row(
+        alignment=ft.MainAxisAlignment.SPACE_AROUND,
+        controls=[
+            ft.Text(
+                f"T{i}",
+                color="white",
+            ) for i in range(1,6)
+        ],
+    )
+    cntInferior.content.controls.append(cntInferiorLinha3)
+
+    #######################################################################
+    #######################################################################
+    cntPrincipal.content.controls.append(cntSuperior)
+    cntPrincipal.content.controls.append(cntCentral)
+    cntPrincipal.content.controls.append(cntInferior)
+
+    page.add(cntPrincipal)
+
+    obterLocalUsuario(None)
+
+
+ft.app(target=main)
